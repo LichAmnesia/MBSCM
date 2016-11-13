ccfd9e600cdb0c0d5ed5f3f579d2981cf4439408
Lich_Amnesia <alwaysxiaop@gmail.com>
2016-11-12 20:05:50 -0700

    fix bugs in HVR_5

--- data/HVR_5/a.py
+++ data/HVR_5/a.py
@@ -1,6 +1,18 @@
 import re
-
+node = set([])
 with open('HVR_5.txt', 'r') as f:
   for line in f.readlines():
-    line = re.sub(r',', ' ', line)
-    print(line),    
+    line = re.sub(r'\n', '', line)
+    a, b = line.split(' ')
+    node.add(a)
+    node.add(b)
+print(node)
+
+with open('HVR_5_metadata.txt', 'r') as f:
+  for line in f.readlines():
+    a, b = line.split(' ')
+    if a not in node:
+      continue
+    print(str(a) + ' ' + str(b)),
+# print(node)
+# print(len(node))
