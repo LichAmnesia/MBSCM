7a0927eb7c56205fbbf1ee4d8429ef5f3b999cd4
LichAmnesia <alwaysxiaop@gmail.com>
2016-11-30 14:13:05 -0700

    change the way to use data

--- data/Reddit/deal_comments.py
+++ data/Reddit/deal_comments.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: LichAmnesia
 # @Date:   2016-11-26 13:20:29
-# @Last Modified by:   LichAmnesia
-# @Last Modified time: 2016-11-26 13:51:24
+# @Last Modified by:   Lich_Amnesia
+# @Last Modified time: 2016-11-30 20:59:35
 # This is to deal with the comments and generate every day's comments file.
 
 import datetime
@@ -12,24 +12,71 @@ dateArray = datetime.datetime.utcfromtimestamp(timeStamp)
 otherStyleTime = dateArray.strftime("%Y-%m-%d")
 print(otherStyleTime)
 
-cnt = 0
-with open('comments', 'r') as fileobject:
-    cur_filename = ''
-    file = None
-    for line in fileobject:
-        js = json.loads(line.replace('\n',''))
-        time_stamp = js['created_utc']
-        date_array = datetime.datetime.utcfromtimestamp(time_stamp)
-        day = date_array.strftime("%Y-%m-%d")
-        filename = 'comments_' + day + '.csv'
-        if filename != cur_filename:
-            if file:
-                file.close()
-            cur_filename = filename
-            file = open(cur_filename, 'w')
-            file.write('day' + '\t' + 'created_utc' + '\t' + 'subreddit_id' + '\t' + 'parent_id' + '\t' + 'author' + '\t' + 'subreddit' + '\t' + 'link_id' + '\t' + 'id' + '\t' + 'retrieved_on' + '\t' + 'score' + '\n')
-        else:
-            file.write(day + '\t' + str(js['created_utc']) + '\t' + js['subreddit_id'] + '\t' + js['parent_id'] + '\t' + js['author'] + '\t' + js['subreddit'] + '\t' + js['link_id'] + '\t' + js['id'] + '\t' + str(js['retrieved_on']) + '\t' + str(js['score']) + '\n')
+
+# 创建每天的comments文件
+def generateComments_date():
+    cnt = 0
+    with open('comments', 'r') as fileobject:
+        cur_filename = ''
+        file = None
+        for line in fileobject:
+            js = json.loads(line.replace('\n',''))
+            time_stamp = js['created_utc']
+            date_array = datetime.datetime.utcfromtimestamp(time_stamp)
+            day = date_array.strftime("%Y-%m-%d")
+            filename = 'comments_' + day + '.csv'
+            if filename != cur_filename:
+                if file:
+                    file.close()
+                cur_filename = filename
+                file = open(cur_filename, 'w')
+                file.write('day' + '\t' + 'created_utc' + '\t' + 'subreddit_id' + '\t' + 'parent_id' + '\t' + 'author' + '\t' + 'subreddit' + '\t' + 'link_id' + '\t' + 'id' + '\t' + 'retrieved_on' + '\t' + 'score' + '\n')
+            else:
+                file.write(day + '\t' + str(js['created_utc']) + '\t' + js['subreddit_id'] + '\t' + js['parent_id'] + '\t' + js['author'] + '\t' + js['subreddit'] + '\t' + js['link_id'] + '\t' + js['id'] + '\t' + str(js['retrieved_on']) + '\t' + str(js['score']) + '\n')
         
 
+# 计算最多subreddit的comment数目
+def countLargestReddit():
+    cnt = 0
+    dic_subreddit = {}
+    with open('comments', 'r') as fileobject:
+        cur_filename = ''
+        file = None
+        for line in fileobject:
+            js = json.loads(line.replace('\n',''))
+            if js['subreddit'] not in dic_subreddit:
+                dic_subreddit[js['subreddit']] = 1
+            else:
+                dic_subreddit[js['subreddit']] += 1
+    ans = sorted(dic_subreddit.items(), key=lambda x: x[1], reverse=True)
+    print(ans[:10])
 
+
+# 创建comment，根据subreddit进行筛选
+def generateComments_subreddit_name(subredditlist=["politics", "GlobalOffensiveTrade"]):
+    cnt = 0
+    with open('comments', 'r') as fileobject:
+        cur_filename = ''
+        file = None
+        for line in fileobject:
+            js = json.loads(line.replace('\n',''))
+            time_stamp = js['created_utc']
+            date_array = datetime.datetime.utcfromtimestamp(time_stamp)
+            day = date_array.strftime("%Y-%m-%d")
+            filename = 'comments_subreddit_' + subredditlist[0] + '_' + subredditlist[1] + '.csv'
+            if filename != cur_filename:
+                if file:
+                    file.close()
+                cur_filename = filename
+                file = open(cur_filename, 'w')
+                file.write('day' + '\t' + 'created_utc' + '\t' + 'subreddit_id' + '\t' + 'parent_id' + '\t' + 'author' + '\t' + 'subreddit' + '\t' + 'link_id' + '\t' + 'id' + '\t' + 'retrieved_on' + '\t' + 'score' + '\n')
+            else:
+                if js['subreddit'] in subredditlist:
+                    file.write(day + '\t' + str(js['created_utc']) + '\t' + js['subreddit_id'] + '\t' + js['parent_id'] + '\t' + js['author'] + '\t' + js['subreddit'] + '\t' + js['link_id'] + '\t' + js['id'] + '\t' + str(js['retrieved_on']) + '\t' + str(js['score']) + '\n')
+
+def main():
+    # countLargestReddit()
+    generateComments_subreddit_name()
+
+if __name__ == '__main__':
+    main()
--- data/Reddit/generate_graph_file.py
+++ data/Reddit/generate_graph_file.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: LichAmnesia
 # @Date:   2016-11-26 13:20:29
-# @Last Modified by:   LichAmnesia
-# @Last Modified time: 2016-11-26 14:53:16
+# @Last Modified by:   Lich_Amnesia
+# @Last Modified time: 2016-11-30 21:06:17
 # This is to deal with the comments and generate every day's comments file.
 
 import datetime
@@ -59,7 +59,46 @@ def read_edge_date(filename='comments_2016-09-01.csv'):
             label_file.write(str(moderators_map[line['moderators']]) + ' ' + str(subreddit_map[line['subreddit']]) + '\n')
     label_file.close()
 
+# 只运算两个subreddit的
+def read_edge_date2(filename='comments_2016-09-01.csv'):
+    edge_data = pd.read_csv(filename, delimiter='\t')
+    edge_map = collections.defaultdict(list)
+    for line_num in range(len(edge_data)):
+        line = edge_data.loc[line_num]
+        # print(line['author'])
+        edge_map[line['link_id']].append(moderators_map[line['author']])
+
+    summary = 0
+    edge_filename = filename.split('.')[0]
+    label_filename = edge_filename + '_metadata' + '.txt'
+    edge_filename = edge_filename + '.txt'
+    edge_file = open(edge_filename, 'w')
+    label_file = open(label_filename, 'w')
+
+    # The node in the edge_list
+    has_set = set([])
+    for i in edge_map:
+        if len(edge_map[i]) > 1:
+            for j in range(len(edge_map[i]) - 1):
+                if edge_map[i][j + 1] != edge_map[i][j]:
+                    has_set.add(edge_map[i][j + 1])
+                    has_set.add(edge_map[i][j])
+                    edge_file.write(str(edge_map[i][j + 1]) + ' ' + str(edge_map[i][j]) + '\n')
+    edge_file.close()
+    print(has_set)
+
+    for line_num in range(len(labels_data)):
+        line = labels_data.loc[line_num]
+        if moderators_map[line['moderators']] in has_set:
+            label_file.write(str(moderators_map[line['moderators']]) + ' ' + str(subreddit_map[line['subreddit']]) + '\n')
+    label_file.close()
+
 
 
 labels_data, moderators_map, subreddit_map = read_label_write_label_file()
-read_edge_date()
+def main():
+    # read_edge_date()
+    read_edge_date(filename='comments_subreddit_politics_GlobalOffensiveTrade.csv')
+
+if __name__ == '__main__':
+    main()
